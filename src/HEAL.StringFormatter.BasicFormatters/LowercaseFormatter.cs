fbbdef105fa42f2ca972f6bf93a2e0ac96ca887d
Stefan Wagner <swagner@heuristiclab.com>
2020-01-26 21:53:05 +0100

    Added license headers

--- src/HEAL.StringFormatter.BasicFormatters/LowercaseFormatter.cs
+++ src/HEAL.StringFormatter.BasicFormatters/LowercaseFormatter.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿#region License Information
+/*
+ * This file is part of HEAL.Bricks.Demo which is licensed under the MIT license.
+ * See the LICENSE file in the project root for more information.
+ */
+#endregion
+
+using System;
 
 namespace HEAL.StringFormatter.BasicFormatters {
   public class LowercaseFormatter : IStringFormatter {
